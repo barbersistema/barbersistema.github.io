3662fa538c4711ee9266333b8d25139dd01e401f
barbersistema <barber.sistema@gmail.com>
2026-04-15 12:06:57 -0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,12 +6,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-com
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-    apiKey: "AIzaSyDmRNuNwNCsJUomjBZkTGb6czCiEvZ0xO4",
-    authDomain: "reactnative-mapsapp-488012.firebaseapp.com",
-    projectId: "reactnative-mapsapp-488012",
-    storageBucket: "reactnative-mapsapp-488012.firebasestorage.app",
-    messagingSenderId: "135444762282",
-    appId: "1:135444762282:web:8766842f893bf41efecad3"
+  apiKey: "AIzaSyDmRNuNwNCsJUomjBZkTGb6czCiEvZ0xO4",
+  authDomain: "reactnative-mapsapp-488012.firebaseapp.com",
+  projectId: "reactnative-mapsapp-488012",
+  storageBucket: "reactnative-mapsapp-488012.firebasestorage.app",
+  messagingSenderId: "135444762282",
+  appId: "1:135444762282:web:8766842f893bf41efecad3"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -19,14 +19,14 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/favicon.ico'
-    };
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
